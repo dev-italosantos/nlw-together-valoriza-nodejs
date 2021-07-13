4a45cc34dcc413d8399804cc2c456de8d74c89cd
Ítalo Santos <italotayllor@gmail.com>
2021-07-13 16:07:22 -0300

    nlw-together-add-controller-tag

---
+++ src/controllers/CreateTagContorller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express"
+import { CreatetagService } from "../services/CreateTagService"
+
+class CreateTagController {
+    async handle(request: Request, response: Response) {
+        const { name } = request.body;
+        
+        const createTagService = new CreatetagService()
+        
+        const tag = await createTagService.execute(name)
+
+        return response.json(tag)
+    }
+}
+
+export { CreateTagController }
